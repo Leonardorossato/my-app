3dd10cb795409d41a998bfd6c0e8d044c97b5fa7
Leonardorossato21 <leonardorossato21@gmail.com>
2023-04-12 11:02:47 -0300

    feat: create sycronizer.plugins.ts

---
+++ src/plugins/sycronizer.plugins.ts
@@ -0,0 +1,8 @@
+import { PluginCommonModule, VendurePlugin } from "@vendure/core";
+import { Customer } from "../entities/customer.entity";
+
+// @VendurePlugin({
+//     entities: [Customer],
+//     imports: [PluginCommonModule],
+//     providers: []
+// })
