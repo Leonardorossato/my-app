c8153553fa304d0468ce5c6355ba019519c6b427
Leonardorossato21 <leonardorossato21@gmail.com>
2023-04-12 09:34:14 -0300

    feat: create apiOptions.config.ts

---
+++ src/config/apiOptions.config.ts
@@ -0,0 +1,22 @@
+import { VendureConfig } from "@vendure/core";
+
+const IS_DEV = process.env.APP_ENV === 'dev';
+
+export const config: VendureConfig['apiOptions'] = {
+    port: process.env.APP_PORT,
+        adminApiPath: 'admin-api',
+        shopApiPath: 'shop-api',
+        // The following options are useful in development mode,
+        // but are best turned off for production for security
+        // reasons.
+        ...(IS_DEV ? {
+            adminApiPlayground: {
+                settings: { 'request.credentials': 'include' } as any,
+            },
+            adminApiDebug: true,
+            shopApiPlayground: {
+                settings: { 'request.credentials': 'include' } as any,
+            },
+            shopApiDebug: true,
+        } : {}),
+}
