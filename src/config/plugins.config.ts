812eb3b5757b88cafa9fc753412db9f6f0d5fe23
Leonardorossato21 <leonardorossato21@gmail.com>
2023-04-12 09:50:15 -0300

    feat: create plugins.config.ts plugins added

---
+++ src/config/plugins.config.ts
@@ -0,0 +1,39 @@
+import { AdminUiPlugin } from "@vendure/admin-ui-plugin";
+import { AssetServerPlugin } from "@vendure/asset-server-plugin";
+import { DefaultJobQueuePlugin, DefaultSearchPlugin, VendureConfig } from "@vendure/core";
+import { EmailPlugin, defaultEmailHandlers } from "@vendure/email-plugin";
+import * as path from "path";
+
+const IS_DEV = process.env.APP_ENV === 'dev';
+
+export const pluginsOptionsConfig: VendureConfig['plugins'] = [
+    AssetServerPlugin.init({
+        route: 'assets',
+        assetUploadDir: path.join(__dirname, '../static/assets'),
+        // For local dev, the correct value for assetUrlPrefix should
+        // be guessed correctly, but for production it will usually need
+        // to be set manually to match your production url.
+        assetUrlPrefix: IS_DEV ? undefined : 'https://www.my-shop.com/assets',
+    }),
+    DefaultJobQueuePlugin.init({ useDatabaseForBuffer: true }),
+    DefaultSearchPlugin.init({ bufferUpdates: false, indexStockStatus: true }),
+    EmailPlugin.init({
+        devMode: true,
+        outputPath: path.join(__dirname, '../static/email/test-emails'),
+        route: 'mailbox',
+        handlers: defaultEmailHandlers,
+        templatePath: path.join(__dirname, '../static/email/templates'),
+        globalTemplateVars: {
+            // The following variables will change depending on your storefront implementation.
+            // Here we are assuming a storefront running at http://localhost:8080.
+            fromAddress: '"example" <noreply@example.com>',
+            verifyEmailAddressUrl: 'http://localhost:8080/verify',
+            passwordResetUrl: 'http://localhost:8080/password-reset',
+            changeEmailAddressUrl: 'http://localhost:8080/verify-email-address-change'
+        },
+    }),
+    AdminUiPlugin.init({
+        route: 'admin',
+        port: process.env.ADMIN_PORT,
+    }),
+]
