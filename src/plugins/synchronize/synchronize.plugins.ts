67bde55b7b8b7f310e4c89714e2b765a335bb02b
Leonardorossato <leonardorossato21@gmail.com>
2023-04-18 15:41:36 -0300

    feat: create synchronize.plugins.ts

---
+++ src/plugins/synchronize/synchronize.plugins.ts
@@ -0,0 +1,17 @@
+import { PluginCommonModule, VendurePlugin } from "@vendure/core";
+import { OnApplicationBootstrap } from "@nestjs/common";
+import { InitEntity } from "../entities/init.entity";
+
+@VendurePlugin({
+    entities: [InitEntity],
+    imports: [PluginCommonModule],
+    providers: []
+})
+
+export class SynchronizationPlugin implements OnApplicationBootstrap {
+    constructor() {}
+  
+    async onApplicationBootstrap() {
+    }
+}
+  
