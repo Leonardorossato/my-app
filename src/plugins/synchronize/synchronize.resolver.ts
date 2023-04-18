0eb665774a41630da01d70630c6bd89d6d28a39e
Leonardorossato <leonardorossato21@gmail.com>
2023-04-18 15:41:43 -0300

    feat: create synchronize.resolver.ts

---
+++ src/plugins/synchronize/synchronize.resolver.ts
@@ -0,0 +1,37 @@
+import { Mutation, Resolver } from "@nestjs/graphql";
+
+@Resolver()
+export class SynchronizationResolverAdmin {
+  constructor() { }
+
+//   @Mutation()
+//   async synchronizationProducts() {
+//     this.productsService.workerProducts();
+//     return "Synchronization products";
+//   }
+
+//   @Mutation()
+//   async synchronizationCollections() {
+//     this.collectionsServices.workerCollections();
+//     return "Synchronization collections";
+//   }
+
+//   @Mutation()
+//   async synchronizationFacets() {
+//     this.facetsServices.workerFacets();
+//     return "Synchronization facets";
+//   }
+
+//   @Mutation()
+//   async synchronizationCustomers() {
+//     this.customersServices.workerCustomers();
+//     return "Synchronization customers";
+//   }
+
+//   @Mutation()
+//   async synchronizationAssets() {
+//     this.assetsServices.workerAssets();
+//     return "Synchronization Assets";
+//   }
+
+}
